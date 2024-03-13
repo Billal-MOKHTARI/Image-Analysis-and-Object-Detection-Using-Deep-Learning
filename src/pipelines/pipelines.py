9f65a08ca4f1ade5f062a4018eca5b895cc2eb51
Billal MOKHTARI <100322125+Billal-MOKHTARI@users.noreply.github.com>
2024-03-13 02:04:08 +0000

    Add visualization file

--- src/pipelines/pipelines.py
+++ src/pipelines/pipelines.py
@@ -1,12 +1,22 @@
 import sys
 import os
+# Importing user-defined module
 sys.path.append(os.path.join("..", "utils"))
 import utils
 import pandas as pd
-import numpy as np
 
-def get_statistics(annot_mat, label_categories_path):
-    # Read label_categories
+def create_object_co_occurrence_graphxr_dataset(annot_mat, label_categories_path):
+    """
+    Creates a dataset for object co-occurrence graph XR based on annotation matrix and label categories.
+
+    Parameters:
+        annot_mat (DataFrame): The annotation matrix.
+        label_categories_path (str): Path to the label categories file.
+
+    Returns:
+        DataFrame: Object-image co-occurrence adjacency list dataset.
+    """
+    # Read label categories
     label_categories = pd.read_csv(label_categories_path, header=0, index_col=0)
 
     # Create adjacency co-occurrence matrix and list
@@ -18,7 +28,6 @@ def get_statistics(annot_mat, label_categories_path):
     node_weight_df.drop(columns=["Neighbor"], inplace=True)
     node_weight_df.rename(columns={"Weight": "nodeWeight"}, inplace=True)
 
-
     # Add the types of objects and their occurrence frequency
     adj_co_occ_list = pd.merge(adj_co_occ_list, label_categories, on="Node", how="left")
     adj_co_occ_list = pd.merge(adj_co_occ_list, node_weight_df, on="Node", how="left")
@@ -26,19 +35,10 @@ def get_statistics(annot_mat, label_categories_path):
     # Create annotation list
     annot_list = utils.matrix_to_list(annot_mat).rename(columns={"Node":"Image", "Neighbor": "Node"})
 
-    # Create a dataset
+    # Create a dataset by adding the images in which each object occurs
     obj_img_co_occ_adj_list = pd.merge(adj_co_occ_list, 
                                        annot_list.drop(columns=["Weight"]), 
                                        on="Node", 
                                        how="right")
 
     return obj_img_co_occ_adj_list
-
-df = pd.read_csv("../../data/output/2. processed_annotations.csv", header=0, index_col=0)
-
-print(get_statistics(df, "../../data/output/label_categories.csv"))
-
-    
-
-
-
