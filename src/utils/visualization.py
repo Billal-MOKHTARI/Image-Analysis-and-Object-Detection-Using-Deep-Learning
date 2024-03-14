1194714740fcde10ce5f049283b8cc5f839f01b3
Billal MOKHTARI <100322125+Billal-MOKHTARI@users.noreply.github.com>
2024-03-14 00:04:18 +0000

    Update visualization.py

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -53,3 +53,59 @@ def show_distribution(data: pd.DataFrame, col: str, grouped_by:str=None, xlabel:
         plt.savefig(save_path, dpi=dpi)
         
     plt.show()
+    
+    
+def show_measure(data, col_x, col_y, grouped_by=None, ascending=None, xlabel=None, ylabel=None, title=None, save_path=None, **kwargs):
+    """
+    Plot a measure against a categorical variable, optionally grouped by another categorical variable.
+
+    Parameters:
+        data (DataFrame): The DataFrame containing the data.
+        col_x (str): The column name for the categorical variable on the x-axis.
+        col_y (str): The column name for the measure to be plotted on the y-axis.
+        grouped_by (str, optional): The column name to group the data by. Defaults to None.
+        ascending (bool, optional): Whether to sort the data in ascending order. Defaults to None.
+        xlabel (str, optional): Label for the x-axis. Defaults to None.
+        ylabel (str, optional): Label for the y-axis. Defaults to None.
+        save_path (str, optional): Filepath to save the plot. Defaults to None.
+        **kwargs: Additional keyword arguments to customize the plot.
+
+    Returns:
+        None
+    """
+    figsize = kwargs.get("figsize", (10, 6))
+    rotation = kwargs.get("rotation", 90)
+    dpi = kwargs.get("dpi", 300)
+
+    if (ascending is not None) and (grouped_by is None):
+        data = data.sort_values(by=col_y, ascending=ascending)
+
+    elif ascending is not None:
+        data = data.groupby(grouped_by).apply(lambda x: x.sort_values(by=col_y, ascending=ascending))
+
+    elif grouped_by is not None:
+        data = data.groupby(grouped_by).apply(lambda x: x)
+
+    # Define colormap based on frequency
+    colors = plt.cm.viridis(data[col_y])
+    # Plotting
+    plt.figure(figsize=figsize)
+    bars = plt.bar(data[col_x], data[col_y], color=colors)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xticks(rotation=rotation)
+    plt.title(title)
+
+    # Adding color bar legend
+    sm = plt.cm.ScalarMappable(cmap='viridis')
+    sm.set_array([])
+    plt.colorbar(sm, label=ylabel, ax=plt.gca())
+
+    plt.tight_layout()
+    if save_path is not None:   
+        plt.savefig(save_path, dpi=dpi)
+
+    plt.show()
+    
+# df = pd.read_csv("../../data/output/6. object_features_with_type.csv", header=0, index_col=0)
+# show_measure(df, "Node", "degree", save_path="t.png")
